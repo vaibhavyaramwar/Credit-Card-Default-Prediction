8393b112d0caacd69f7e7e05179a5b5c8016fe19
Vaibhav Yaramwar <vaibhav.yaramwar@gmail.com>
2022-07-12 18:31:46 +0530

    Docker file and Github workflow added

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return "This a Machine Learning Project for Credit Card Default Prediction"
+    return "This is a Machine Learning Project for Credit Card Default Prediction"
 
 if __name__ == "__main__":
     app.run(debug=True)
