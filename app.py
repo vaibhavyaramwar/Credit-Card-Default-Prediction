d284aa2ddd121fe7883dd7aa17591a5936fe56cc
Vaibhav Yaramwar <vaibhav.yaramwar@gmail.com>
2022-07-13 18:01:15 +0530

    logging and exception modules added

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from creditdefaulter.exception import Credit_Card_Default_Exception
+from creditdefaulter.logger import logging
+import sys
 
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return "This is a Machine Learning Project for Credit Card Default Prediction"
+    try:
+        
+        logging.info("New request received")
+        return "This is a Machine Learning Project for Credit Card Default Prediction"
+    except Exception as e:
+        credit_Card_Default_Exception = Credit_Card_Default_Exception(e,sys)
+        logging.info(credit_Card_Default_Exception.error_message)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ creditdefaulter/exception/__init__.py
@@ -0,0 +1,32 @@
+import os,sys
+
+class Credit_Card_Default_Exception(Exception):
+
+    def __init__(self,error_message:Exception,error_details:sys):
+        super().__init__(error_message)
+        self.error_message = Credit_Card_Default_Exception.get_detailed_error_message(error_message=error_message,error_details=sys)
+    
+    @staticmethod
+    def get_detailed_error_message(error_message:Exception,error_details:sys) -> str:
+        """
+            error_message: Exception Object
+            error_detail: Object of sys module 
+        """
+        _,_,exec_tb = error_details.exc_info()
+        exception_block_line_no = exec_tb.tb_frame.f_lineno
+        try_block_line_no = exec_tb.tb_lineno
+        filename = exec_tb.tb_frame.f_code.co_filename
+
+        error_message = f"""Error Occured in [{filename}] 
+        at try block line no : [{try_block_line_no}] 
+        and exception block line no : [{exception_block_line_no}] 
+        and error_message is : [{error_message}]"""
+        
+        return error_message
+
+    def __str__(self):
+        self.error_message
+
+    def __repr__(self) -> str:
+        return Credit_Card_Default_Exception.__name__.str()
+
